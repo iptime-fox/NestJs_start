258d24e7003a73ad4f53d5eee76da9b505a7d4c0
iptime-fox <dudndi826@gmail.com>
2023-12-10 23:38:01 +0900

    #14.2 Send Message

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -1,25 +1,47 @@
 import type { NextPage } from 'next';
 import Layout from '@components/layout';
 import Message from '@components/message';
+import useSWR from 'swr';
+import { useRouter } from 'next/router';
+import { Stream } from '@prisma/client';
+import { useForm } from 'react-hook-form';
+import useMutation from '@libs/client/useMutation';
+
+interface StreamResponse {
+  ok: true;
+  stream: Stream;
+}
+
+interface MessageForm {
+  message: string;
+}
 
 const Stream: NextPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit, reset } = useForm<MessageForm>();
+  const { data } = useSWR<StreamResponse>(
+    router.query.id ? `/api/streams/${router.query.id}` : null
+  );
+  const [sendMessage, { loading, data: sendMessageData }] = useMutation(
+    `/api/streams/${router.query.id}/messages`
+  );
+  const onValid = (form: MessageForm) => {
+    if (loading) return;
+    reset();
+    sendMessage(form);
+  };
   return (
     <Layout canGoBack>
       <div className='py-10 px-4  space-y-4'>
         <div className='w-full rounded-md shadow-sm bg-slate-300 aspect-video' />
         <div className='mt-5'>
-          <h1 className='text-3xl font-bold text-gray-900'>Galaxy S50</h1>
-          <span className='text-2xl block mt-3 text-gray-900'>$140</span>
-          <p className=' my-6 text-gray-700'>
-            My money&apos;s in that office, right? If she start giving me some
-            bullshit about it ain&apos;t there, and we got to go someplace else
-            and get it, I&apos;m gonna shoot you in the head then and there.
-            Then I&apos;m gonna shoot that bitch in the kneecaps, find out where
-            my goddamn money is. She gonna tell me too. Hey, look at me when
-            I&apos;m talking to you, motherfucker. You listen: we go in there,
-            and that ni**a Winston or anybody else is in there, you the first
-            motherfucker to get shot. You understand?
-          </p>
+          <h1 className='text-3xl font-bold text-gray-900'>
+            {data?.stream?.name}
+          </h1>
+          <span className='text-2xl block mt-3 text-gray-900'>
+            ${data?.stream?.price}
+          </span>
+          <p className=' my-6 text-gray-700'>{data?.stream?.description}</p>
         </div>
         <div>
           <h2 className='text-2xl font-bold text-gray-900'>Live Chat</h2>
@@ -29,9 +51,12 @@ const Stream: NextPage = () => {
             <Message message='미쳤어' />
           </div>
           <div className='fixed py-2 bg-white  bottom-0 inset-x-0'>
-            <div className='flex relative max-w-md items-center  w-full mx-auto'>
+            <form
+              onSubmit={handleSubmit(onValid)}
+              className='flex relative max-w-md items-center  w-full mx-auto'>
               <input
                 type='text'
+                {...register('message', { required: true })}
                 className='shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500'
               />
               <div className='absolute inset-y-0 flex py-1.5 pr-1.5 right-0'>
@@ -39,7 +64,7 @@ const Stream: NextPage = () => {
                   &rarr;
                 </button>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
